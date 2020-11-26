1eb3f6c00cfb6348a3921a50c7f4589252a54d75
alex308248 <alex308248@gmail.com>
2020-11-26 12:24:46 +0800

    try to submit on gradescope

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -30,6 +30,45 @@ public class NBody {
 		return Bodys;
 	}
 	public static void main(String[] args) {
-		
+		Double T = Double.parseDouble(args[0]);
+		Double dt = Double.parseDouble(args[1]);
+		String filename = args[2];
+		double radius = readRadius(filename);
+		Body[] bodies = readBodies(filename);
+		String background = "images/starfield.jpg";
+		StdDraw.enableDoubleBuffering();
+		StdDraw.setScale(-2*radius, 2*radius);
+		StdDraw.clear();
+        StdDraw.picture(0, 0, background);
+        for(int i = 0; i < bodies.length; i++) {
+        	bodies[i].draw();
+        }
+        StdDraw.show();
+        for(double time = 0; time < T; time = time + dt) {
+        	double[] xForce = new double[bodies.length];
+        	double[] yForce = new double[bodies.length];
+        	for(int i = 0; i < bodies.length; i++) {
+        		xForce[i] = bodies[i].calcNetForceExertedByX(bodies);
+        		yForce[i] = bodies[i].calcNetForceExertedByX(bodies);
+        	}
+        	for(int i = 0; i < bodies.length; i++) {
+        	    bodies[i].update(dt, xForce[i], yForce[i]);   
+        	}
+        	StdDraw.setScale(-2*radius, 2*radius);
+    		StdDraw.clear();
+            StdDraw.picture(0, 0, background);
+            for(int i = 0; i < bodies.length; i++) {
+        	    bodies[i].draw();
+            }
+            StdDraw.show();
+            StdDraw.pause(10);
+        }
+        StdOut.printf("%d\n", bodies.length);
+		StdOut.printf("%.2e\n", radius);
+		for (int i = 0; i < bodies.length; i++) {
+    		StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+            bodies[i].xxPos, bodies[i].yyPos, bodies[i].xxVel,
+            bodies[i].yyVel, bodies[i].mass, bodies[i].imgFileName);   
+		}
 	}
 } 

6035926fc3d2de2315dcf2b805ddbfe16de43e4a
Alex <alex308248@gmail.com>
2021-05-19 20:59:16 +0800

    adding space in drawing_a_triangle.java

--- hw0/Drawing_a_Triangle.java
+++ hw0/Drawing_a_Triangle.java
@@ -1,10 +1,10 @@
 public class Drawing_a_Triangle {
     public static void main(String[] args) {
-        for(int i=0;i<5;i++) {
-		for (int j=0;j<=i;j++) {
-			System.out.print("*");
-		}
+        for(int i = 0; i < 5; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
 		System.out.print("\n");
-	}
+		}
     }
 }

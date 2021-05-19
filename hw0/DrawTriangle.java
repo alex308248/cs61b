7d82a1f86d99fc7471dfe29a80376d072f1d8643
Alex <alex308248@gmail.com>
2021-05-19 21:01:29 +0800

    adding space in drawtraingle.java

--- hw0/DrawTriangle.java
+++ hw0/DrawTriangle.java
@@ -1,10 +1,10 @@
 public class DrawTriangle {
    public static void DrawTriangle(int N) {
-      for(int i=0;i<N;i++) {
-         for (int j=0;j<=i;j++) {
+      for(int i = 0; i < N; i++) {
+         for (int j = 0; j <= i; j++) {
             System.out.print("*");
          }
-      System.out.print("\n");
+         System.out.print("\n");
       }
    }
    public static void main(String[] args) {
